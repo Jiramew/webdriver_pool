95f7b324474e37e6e63ce76f6244824ba83126bd
yanghanbing <yanghb@lhcis.com>
2017-11-09 16:20:05 +0800

    Update: test

--- test/test_pool.py
+++ test/test_pool.py
@@ -1,11 +1,15 @@
+import time
 from webdriver_pool.webdriver_pool import WebdriverPool
 from webdriver_pool.webdriver_pool_config import WebDriverPoolConfig
 
 if __name__ == "__main__":
     wdp_config = WebDriverPoolConfig(phantomjs_path="D:/program/phantomjs-2.1.1-windows/bin/phantomjs.exe")
     wd = WebdriverPool(wdp_config)
-    driver = wd.acquire()
-    driver.get("www.baidu.com")
-    wd.release(driver)
+    for i in range(10):
+        driver = wd.acquire()
+        print(driver.session_id)
+        driver.get("www.baidu.com")
+        wd.release(driver)
+        time.sleep(1)
     wd.stop()
     print(wd.acquire() is None)
